6ce39759852a6c4838c50cf6f96b5924905fbbce
Andrey <llianandr94@gmail.com>
2021-11-12 22:53:44 +0200

    Task of Rendering actors cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,40 @@
+'use strict';
 
-new Map().set('www.facebook.com','fab fa-facebook');
+const actorsList = document.getElementById('actorsList');
+const choosedList = document.querySelector('.choosedList');
+
+const messagesClasses = new Map();
+
+messagesClasses.set('twitter.com', 'fab fa-twitter');
+messagesClasses.set('www.facebook.com', 'fab fa-facebook');
+messagesClasses.set('www.instagram.com', 'fab fa-instagram');
+
+
+fetch('./assets/js/data.json').then((response)=> response.json()).then((actors)=>{
+  actors.map((actor)=>actorsList.append(createCard(actor)));
+})
+
+
+function createCard(actor){
+  const {id, firstName, lastName, profilePicture:url, contacts} = actor;
+  const fullName = getFullName(firstName, lastName);
+  const card = createElement('li', {attributes:{'data-id':id,'data-full-name':fullName},classNames:['cardWrapper'],events:{'click':clickHanler}},
+    createElement('article',{classNames:['cardContainer']},
+      createElement('div',{classNames:['cardImageWrapper']},
+        createElement('div',{classNames:['initials'],styleProreties:{'background':stringToColour(fullName)}}, document.createTextNode(getFirstLetterInitials(fullName) || 'none')),
+        createElement('img',{attributes:{'src':url},classNames:['cardImage'], events:{'error':handlerImageError}})
+      ),
+      createElement('h2',{classNames:['cardName']}, document.createTextNode(fullName || 'unknow')),
+      createElement('div',{classNames:['messangersWrapper']},
+          ...(contacts.map((href)=>
+              createElement('a', {attributes:{'href':href}, classNames:findingClassByUrl(messagesClasses, href)})
+              ))         
+        ), 
+    )
+  )
+  return card; 
+}
+
 
 
 
@@ -7,13 +42,13 @@ new Map().set('www.facebook.com','fab fa-facebook');
  * 
  * @param {string} type 
  * @param {object} options 
- * @param {string} options.typeEvent
+ * @param {object} options.attributes
  * @param {string[]} options.classNames
- * @param {function} options.onClick
+ * @param {object} options.events
  * @param {Node[]} children 
  * return {Node}
  */
- function createElement(type='div',{attributes={}, classNames=[], events={}}, ...children){
+ function createElement(type='div',{attributes={}, classNames=[], events={}, styleProreties={}}, ...children){
   const elem = document.createElement(type);
   for(const [attrName, attrValue] of Object.entries(attributes)){
     elem.setAttribute(attrName, attrValue);
@@ -21,7 +56,56 @@ new Map().set('www.facebook.com','fab fa-facebook');
   for(const [eventType, eventHandler] of Object.entries(events)){
     elem.addEventListener(eventType, eventHandler);
   }
+  for (const [property, value] of Object.entries(styleProreties)) {
+    elem.style[property] = value;
+  }
   elem.classList.add(...classNames);
   elem.append(...children);
   return elem;
+}
+
+
+function clickHanler({currentTarget:{dataset:{id,fullName}}}){
+  const arrOfLiDataNames = [...choosedList.children].map((li)=>li.dataset.id);  
+  if(!arrOfLiDataNames.includes(id,1)){
+    choosedList.append(createElement('li', {attributes:{"data-id":id}}, document.createTextNode(fullName)));
+  }
+}
+
+function handlerImageError({target}){
+  console.log(target);
+  target.remove();
+}
+
+function getFullName(name, sname){
+  return name + ' ' + sname;
+}
+
+function getFirstLetterInitials (str){  
+  let newStr = '';
+  for (const element of str.split(' ')) {
+    newStr += element.slice(0,1).toUpperCase();
+  }
+  return newStr;
+}
+function findingClassByUrl(map, str){  
+  for (const key of map.keys()) {
+    if(key.includes(new URL(str).hostname)){
+      return map.get(key).split(' ');      
+    }    
+  }  
+}
+
+
+function stringToColour(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
 }
